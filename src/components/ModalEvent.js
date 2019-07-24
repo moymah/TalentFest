6dc98bfecceecf3e129162fc7f81886213b49186
ligiallucca <lilillucca@gmail.com>
2019-07-24 11:54:37 -0300

    css-event

--- src/components/ModalEvent.js
+++ src/components/ModalEvent.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react';
 import firebase from '../firebaseConfig';
 import UploadImage from './UploadImage';
+import './ModalEvent.css'
 
 export default function () {
     const [eventName, setEventName] = useState('');
@@ -8,7 +9,7 @@ export default function () {
     const [currentUser, setCurrentUser] = useState('');
 
     useEffect(() => {
-       setCurrentUser(firebase.auth().currentUser)
+        setCurrentUser(firebase.auth().currentUser)
     },[])
 
     useEffect(() => {
@@ -20,7 +21,7 @@ export default function () {
     function timeNow() {
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit'};
         return new Date().toLocaleDateString('pt-BR', options)
-      }
+        }
 
     
 function sendEventToFirebase() {
@@ -36,14 +37,19 @@ function sendEventToFirebase() {
         alert("Oops.")
     )
 }
-    
+    //só para conseguir commitar//
     return(
-        <section>
-        <input type="text" onChange={(event) => setEventName(event.target.value)}placeholder="Nome do Evento" id="event"/>
-        <textarea cols="40" onChange={(event) => setDescription(event.target.value)} rows="6" placeholder="Descrição"  id="description"/>
-        <input type="file" id="photo" onChange={(event) => UploadImage(event.target.files) }/>
-        <button onClick={() => sendEventToFirebase()}>Criar Evento</button> 
-        </section>
+        <form> 
+            <label for="event-name" class="event-labels">Nome</label>    
+            <input  type="text" className="event-input" onChange={(event) => setEventName(event.target.value)}placeholder="Nome do Evento" id="event"/>
+            <label for="event-description" class="event-labels">Descrição</label> 
+            <textarea cols="40" className="event-input" onChange={(event) => setDescription(event.target.value)} rows="6" placeholder="Descrição"  id="description"/>
+            <div className="container photo-container">
+            <label for="event-photo" class="event-labels">Insira uma foto:</label> 
+            <input type="file" className="file" id="photo" onChange={(event) => UploadImage(event.target.files) }/>
+            </div>
+            <button className="event-button" onClick={() => sendEventToFirebase()}>Criar Evento</button>
+        </form>
     )
 
 }
