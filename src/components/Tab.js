5c841cd054127cb1a155bfd9461a67eedc0932db
ligiallucca <lilillucca@gmail.com>
2019-07-25 17:03:06 -0300

    implemento do react-materialize e começando a mudar o layout das tabs

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import './Tab.css';
+/*import './Tab.css';*/
+import {Tabs} from 'react-materialize';
 
 function TabLogin (props){
     return (
-        <div>
-        <input type="radio" id={props.id} defaultChecked={props.checked} className="input_tab" name="tabs"/> 
+        <Tabs>
+        <div className = "tab-demo z-depth-1" options={{swipeable: true}}>
+        <input type="radio" class="tab" id={props.id} defaultChecked={props.checked} className="active" name="tabs"/> 
         <label htmlFor={props.id} className="tab_label">
             {props.text}
         </label>
         <div className={props.classNameContent}>
                 {props.children}
-              </div>
         </div>
+        </div>
+        </Tabs>
     );
 }
 
